1020d23fad82fb39a1ba9011ae9e240f33fdad02
Your Name <you@example.com>
2025-12-16 09:13:40 +0200

    Fix Vercel pg module loading

--- database.js
+++ database.js
@@ -4,6 +4,7 @@ const path = require('path');
 // Initialize Database (PostgreSQL)
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectModule: require('pg'),
     dialectOptions: {
         ssl: {
             require: true,
